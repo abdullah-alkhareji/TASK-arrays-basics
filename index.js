67120d8bc7bc52a2a4415ac095d441aba5964e82
abdullah-alkhareji <outsiderandco@gmail.com>
2022-01-10 18:22:06 +0300

    fixed the layout

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// 3. Create an empty array `emptyArray`
 const emptyArray = [];
+
+// 4. Create an array `hobbies`, add 6 hobbies to it
 let hobbies = [
 	"playing video games",
 	"watching Movies",
@@ -7,16 +10,33 @@ let hobbies = [
 	"coding",
 	"road trips",
 ];
-const numbers = [2, 7, 5, 1, 9];
-console.log(`6-  ${numbers[1]}`);
-console.log(`7-  ${numbers[numbers.length - 1]}`);
-hobbies.push("swimming");
-console.log(`9-  ${hobbies[hobbies.length - 1]}`);
-console.log(`10- ${hobbies.length}`);
 
+// 5. Create an array `numbers`, add your favorite 5 numbers
+const numbers = [2, 7, 5, 1, 9];
+
+// 6. Print out the second number from the array `numbers`
+console.log(numbers[1]);
+
+// 7. Print out the last hobby from the array `hobbies`
+console.log(numbers[numbers.length - 1]);
+
+// 8. Add a fake hobby to the end of the array `hobbies`
+hobbies.push("swimming");
+
+// 9. Print the last hobby
+console.log(hobbies[hobbies.length - 1]);
+
+// 10. Print the length of array `hobbies`
+console.log(hobbies.length);
+
+// 11. Remove the last 2 hobbies from array `hobbies`
 hobbies.splice(-2, 2);
-console.log(`12- ${hobbies.length}`);
-console.log(`13- ${hobbies}`);
+
+// 12. Print length of `hobbies`
+console.log(hobbies.length);
+
+// 13. Print every element in hobbies in a single line
+console.log(`${hobbies}`);
 
 // console.log(hobbies[0]);
 // console.log(hobbies[1]);
@@ -24,5 +44,6 @@ console.log(`13- ${hobbies}`);
 // console.log(hobbies[3]);
 // console.log(hobbies[4]);
 
+// 14. Empty the array `hobbies`
 hobbies = [];
 console.log(hobbies);
